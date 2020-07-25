6509e9468b015ead732900af543b96ef6bab59fa
Your NameBrunoCauan <brunocaua2000@gmail.com>
2020-07-25 13:58:18 -0300

    First Commit

---
+++ src/api.js
@@ -0,0 +1,4 @@
+export default {
+    baseUrl: '//free.currconv.com/api/v7',
+    key: 'apiKey=0f9a2d708a7604e0040f',
+}
---
+++ src/components/CurrencySelector/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./styles.css";
+
+class CurrencySelector extends React.Component {
+  currencyMapping() {
+    return this.props.currencyList.map((currentCurrency, index) => (
+      <option value={currentCurrency} key={index}>
+        {currentCurrency}
+      </option>
+    ));
+  }
+
+  render() {
+    return (
+      <select value={this.props.value} onChange={(e) => this.props.updateCurrency(e.target.value) }>
+        {this.currencyMapping()}
+      </select>
+    );
+  }
+}
+
+export default CurrencySelector;
